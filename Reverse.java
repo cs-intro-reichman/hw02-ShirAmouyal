ff6ea9164521d0f852faccddcccf16448ef7c703
Avielni222 <155298746+Avielni222@users.noreply.github.com>
2024-01-05 17:49:41 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -7,17 +7,21 @@ public class Reverse {
     int leng=str.length();
     if(leng%2==0)
     {
+	// bad indentation 
     middle= str.charAt(leng/2-1); 
     }
     else
     {
+	 // bad indentation 
     middle= str.charAt(leng/2); 
     }  
     for (int i=leng-1; i>=0;i--)
     {
+	// bad indentation 
      reverse+=str.charAt(i);
     }
     System.out.println(reverse);
     System.out.println("The middle character is "+middle);
+// bad indentation of brackets	
 	}
 }
